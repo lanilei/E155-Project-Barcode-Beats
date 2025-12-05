0447204cec4ad9240c43e62dd60de9b171f99fe0
Leilani <157073466+lanilei@users.noreply.github.com>
2025-12-04 20:47:29 -0800

    Update filter_spi.sv

--- filter_spi.sv
+++ filter_spi.sv
@@ -28,7 +28,8 @@ module filter_spi(input  logic sck, 			// Spi clk
 				tuning_word_captured <= {tuning_word_captured[30:0], 1'b0};
 			end
 			sck_count <= sck_count + 1'b1;
-		end                                     	
+		end
+                                     	
 			
 	// send sdo on the negative edge of sck
     always_ff @(negedge sck)
